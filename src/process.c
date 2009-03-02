f3a980f681bc288cd5555ea04317da578d1156ad
Simon Arlott <simon.arlott.org.uk>
2009-03-02 10:42:32 +0000

    update minimum silence time and add save/test code

--- src/process.c
+++ src/process.c
@@ -9,10 +9,21 @@
 
 static unsigned long long last = ~0;
 
+#define SAVE 1
+
 void process_input(unsigned char *buf, int buflen, unsigned long long time, unsigned long long persample) {
 	int i;
 	unsigned int min = ~0, max = 0, ring = 0;
 	unsigned long long now = time - persample*buflen;
+
+#if SAVE
+	int fd = open("save", O_WRONLY|O_CREAT|O_TRUNC, 0644);
+	if (fd) {
+		write(fd, buf, buflen);
+		close(fd);
+	}
+#endif
+
 	for (i = 0; i < buflen; i++) {
 		if (buf[i] < min)
 			min = buf[i];
@@ -23,7 +34,7 @@ void process_input(unsigned char *buf, int buflen, unsigned long long time, unsi
 
 		if (buf[i] < 126 || buf[i] > 130) {
 			/* ding dong! */
-			if (now > last && now - last >= 200000) {
+			if (now > last && now - last >= 400000) {
 				ring++;
 
 				printf(", <fork>");
@@ -45,7 +56,17 @@ void process_input(unsigned char *buf, int buflen, unsigned long long time, unsi
 			last = now;
 		}
 	}
+
+#if SAVE
+	if (min != 128 || max != 128) {
+		char fname[21];
+		snprintf(fname, 20, "%llu", time - persample*buflen);
+		rename("save", fname);
+	}
+#endif
+
 	if (last == ~0)
 		last = 0;
+
 	printf(", min=%d, max=%d, last=%llu, ring=%d\n", min, max, last, ring);
 }
