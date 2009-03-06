fac5953f0003c4190145349e640ed66f50f0dd3c
Simon Arlott <simon.arlott.org.uk>
2009-03-06 13:07:51 +0000

    update copyright/licence

--- src/time.c
+++ src/time.c
@@ -1,6 +1,19 @@
-// Get current time as unsigned long long to the nearest microsecond
-// (on 64-bit systems this should be changed to use unsigned long)
-// ©2005 Simon Arlott
+/*
+ * Copyright ©2005  Simon Arlott
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (Version 2) as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <sys/time.h>
 #include <unistd.h>
