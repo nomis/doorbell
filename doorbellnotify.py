aec1222e8a2808a7ea108995f43c51f0d18be8c0
Simon Arlott <simon.arlott.org.uk>
2010-11-25 21:18:57 +0000

    add notify exec process

---
+++ doorbellnotify.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from __future__ import print_function
+import argparse
+import daemon
+import notifylib
+import os
+import subprocess
+import sys
+
+EXIT_SUCCESS, EXIT_FAILURE = range(0, 2)
+
+class Execer:
+	def __init__(self, cmd):
+		self.cmd = cmd
+		self.log = notifylib.Log("doorbellnotify")
+
+	def ring(self, ts):
+		try:
+			process = subprocess.Popen([self.cmd, ts], stdin=open(os.devnull), stdout=open(os.devnull), stderr=subprocess.PIPE)
+			stdout, stderr = process.communicate()
+			print("Return code: {0}".format(process.returncode))
+		
+			self.log("{0} [{1}]".format(ts, process.returncode))
+			for msg in stderr.split("\n"):
+				self.log("  {0}".format(msg))
+			return process.returncode == EXIT_SUCCESS
+		except OSError, e:
+			print(e)
+
+			self.log("{0} {1}".format(ts, e))
+			return False
+
+class NotifyExec(notifylib.Handler):
+	def __init__(self, db, doorbell, cmd):
+		notifylib.Handler.__init__(self, db, doorbell)
+		self.execer = Execer(cmd)
+
+	def handle_ring(self, ts):
+		return self.execer.ring(ts)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Exec notify on doorbell rings')
+	parser.add_argument('-d', '--daemon', action='store_true', help='Run in the background')
+	parser.add_argument('doorbell', help='Doorbell identifier')
+	parser.add_argument('cmd', help='Command name')
+	args = parser.parse_args()
+
+	db = notifylib.DB()
+	notify = NotifyExec(db, args.doorbell, args.cmd)
+
+	if args.daemon:
+		with daemon.DaemonContext():
+			notify.main_loop()
+	else:
+		notify.main_loop()
+
+	sys.exit(EXIT_FAILURE)
---
+++ notifylib.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from __future__ import division
+from __future__ import print_function
+import datetime
+import pg
+import pgdb
+import select
+import sys
+import syslog
+import time
+
+class Rings:
+	class NoSuchDoorbell(Exception):
+		pass
+
+	def __init__(self, db, doorbell):
+		data = db.select1("SELECT id FROM doorbells WHERE id = %(id)s", { "id": doorbell })
+		if data is None:
+			raise self.NoSuchDoorbell(doorbell)
+
+		self.doorbell = doorbell
+		self.db = db
+
+	def get_next(self):
+		print("Getting ring...")
+		data = self.db.select1("SELECT start FROM dingdong WHERE doorbell = %(id)s AND notify = 'f' ORDER BY start LIMIT 1", { "id": self.doorbell })
+
+		if data is not None and data[0] is not None:
+			print("Ring: {0}".format(data[0]))
+			return data[0]
+		return None
+
+	def mark_notified(self, ring):
+		return self.db.update("UPDATE dingdong SET notify = 't' WHERE doorbell = %(id)s AND start = %(ring)s", { "id": self.doorbell, "ring": ring })
+
+class DB:
+	class Reconnect(Exception):
+		pass
+
+	def __init__(self):
+		self.db = None
+
+	def abort(self, e=None):
+		if e is not None:
+			print(e, file=sys.stderr)
+		if self.db is not None:
+			try:
+				self.db.close()
+			except pg.DatabaseError:
+				pass
+		self.db = None
+
+	def connect(self):
+		try:
+			if self.db is None:
+				print("Connecting to DB...")
+				self.db = pgdb.connect()
+				self.listen()
+				self.commit()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			return False
+		except self.Reconnect:
+			return False
+		else:
+			return True
+
+	def listen(self):
+		try:
+			c = self.db.cursor()
+			c.execute("SET TIME ZONE 0")
+			c.execute("LISTEN changed")
+			c.close()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+
+	def select(self, query, data):
+		if not self.connect():
+			raise self.Reconnect
+		try:
+			c = self.db.cursor()
+			c.execute(query, data)
+			data = c.fetchall()
+			c.close()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+		else:
+			return data
+
+	def select1(self, query, data):
+		data = self.select(query, data)
+		return data[0] if len(data) > 0 else None
+
+	def update(self, query, data):
+		if not self.connect():
+			raise self.Reconnect
+		try:
+			c = self.db.cursor()
+			c.execute(query, data)
+			rows = c.rowcount
+			c.close()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+		else:
+			return rows
+
+	def commit(self):
+		if self.db is None:
+			raise self.Reconnect
+		try:
+			self.db.commit()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+
+	def rollback(self):
+		if self.db is None:
+			raise self.Reconnect
+		try:
+			self.db.rollback()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+
+	def wait(self):
+		if not self.connect():
+			raise self.Reconnect
+		try:
+			notify = self.db._cnx.getnotify()
+			if notify is None:
+				print("Listening...")
+
+			while notify is None:
+				if self.db._cnx.fileno() < 0:
+					raise self.Reconnect
+				(l, r, x) = select.select([self.db._cnx], [], [self.db._cnx], 0)
+				notify = self.db._cnx.getnotify()
+			print("Notified")
+		except self.Reconnect:
+			self.abort()
+			raise self.Reconnect
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+
+class Log:
+	def __init__(self, name):
+		syslog.openlog(name)
+
+	def __call__(self, message):
+		syslog.syslog(message)
+
+class Handler:
+	def __init__(self, db, doorbell):
+		self.db = db
+		self.rings = Rings(db, doorbell)
+
+	def handle_ring(self, ts):
+		pass
+
+	def process_ring(self):
+		ring = self.rings.get_next()
+		if ring is None:
+			self.db.commit()
+			return None
+		else:
+			self.rings.mark_notified(ring)
+
+			ok = self.handle_ring(ring)
+			try:
+				if ok:
+					self.db.commit()
+				else:
+					self.db.rollback()
+			except DB.Reconnect:
+				ok = False
+
+			return ok
+
+	def wait_for_ring(self):
+		self.db.wait()
+
+	def main_loop(self):
+		while True:
+			try:
+				ok = self.process_ring()
+				if ok != False:
+					self.wait_for_ring()
+				else:
+					time.sleep(60)
+			except DB.Reconnect:
+				time.sleep(5)
+				while not self.db.connect():
+					time.sleep(5)
